1d04569c5210d2c367f868d6513d1fcc10de2ee3
Lokesh Pandit <lokesh73502@gmail.com>
2024-12-17 21:23:06 +0530

    all done

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+
 import LoadingSpinner from "../../components/common/LoadingSpinner";
 
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
-import toast from "react-hot-toast";
 
 const NotificationPage = () => {
-
 	const queryClient = useQueryClient();
 	const { data: notifications, isLoading } = useQuery({
 		queryKey: ["notifications"],
@@ -16,50 +16,36 @@ const NotificationPage = () => {
 			try {
 				const res = await fetch("/api/notifications");
 				const data = await res.json();
-
-				if(!res.ok){
-					throw new Error(data.error || "Something went wrong"); 
-				}
-
+				if (!res.ok) throw new Error(data.error || "Something went wrong");
 				return data;
-			} 
-			catch (error) {
+			} catch (error) {
 				throw new Error(error);
 			}
 		},
 	});
 
-	const { mutate: deleteNotifications, isPending: isCommenting } = useMutation({
+	const { mutate: deleteNotifications } = useMutation({
 		mutationFn: async () => {
 			try {
 				const res = await fetch("/api/notifications", {
 					method: "DELETE",
 				});
-
 				const data = await res.json();
 
-				if(!res.ok){
-					throw new Error(data.error || "Something went wrong"); 
-				}
-
+				if (!res.ok) throw new Error(data.error || "Something went wrong");
 				return data;
-			} 
-			catch (error) {
+			} catch (error) {
 				throw new Error(error);
 			}
 		},
 		onSuccess: () => {
-			toast.success("Notifications deleted successfully!");
-			queryClient.invalidateQueries({queryKey: ["notifications"]});
+			toast.success("Notifications deleted successfully");
+			queryClient.invalidateQueries({ queryKey: ["notifications"] });
 		},
 		onError: (error) => {
 			toast.error(error.message);
-		}
-	})
-
-	// const deleteNotifications = () => {
-	// 	alert("All notifications deleted");
-	// };
+		},
+	});
 
 	return (
 		<>
