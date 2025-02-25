a007b3ca4609184456106e1a19b996079a056c46
Lokesh Pandit <lokesh73502@gmail.com>
2025-02-25 17:23:54 +0530

    Deployment added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 // creating express server 
 import express, { urlencoded } from "express";
+import path from "path"
 import dotenv from "dotenv";
 import { connect } from "mongoose";
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -22,6 +23,7 @@ cloudinary.config({
 
 const app = express();
 const PORT= process.env.PORT || 5000;
+const __dirname = path.resolve()
 
 //Middle layers
 app.use(express.json({limit: "10mb"})); //to parse req.body in auth.controllers.js(limit should not be too large 
@@ -36,6 +38,14 @@ app.use("/api/auth", authRoutes) ;
 app.use("/api/users", userRoutes) ;
 app.use("/api/posts", postRoutes) ;
 app.use("/api/notifications", notificationRoutes) ;
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res)=>{
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 
 app.listen(PORT,()=>{
